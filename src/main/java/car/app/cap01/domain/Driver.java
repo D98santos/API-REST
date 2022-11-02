cb026c74cb718f0beb718e263655e4b22db60e60
D98santos <ds.99102811@gmail.com>
2022-11-02 06:24:33 -0300

    inicio

---
+++ src/main/java/car/app/cap01/domain/Driver.java
@@ -0,0 +1,16 @@
+package car.app.cap01.domain;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Driver {
+    
+    @Id
+    Long id;
+    String name;
+    Date birthDate;
+}
